b918e4075f4a5f6613de6e47eb6f8f9217da09d6
H_천태경 <cjsxorud111@gmail.com>
2020-02-19 09:50:23 +0900

    스프링부트aws웹서비스 실습

--- src/main/java/com/socicalc/service/posts/PostsService.java
+++ src/main/java/com/socicalc/service/posts/PostsService.java
@@ -2,12 +2,16 @@ package com.socicalc.service.posts;
 
 import com.socicalc.domain.posts.Posts;
 import com.socicalc.domain.posts.PostsRepository;
+import com.socicalc.web.dto.PostsListResponseDto;
 import com.socicalc.web.dto.PostsResponseDto;
 import com.socicalc.web.dto.PostsSaveRequestDto;
 import com.socicalc.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -34,5 +38,12 @@ public class PostsService {
                 .orElseThrow(() -> new IllegalArgumentException("해당 사용자가 없습니다. id=" + id));
 
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
     }
 }
