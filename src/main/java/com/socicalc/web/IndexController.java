b8593e539117ea8c0a4483fb58d589dd1bda47a0
H_천태경 <cjsxorud111@gmail.com>
2020-03-12 14:36:18 +0900

    /uri 로직삭제

--- src/main/java/com/socicalc/web/IndexController.java
+++ src/main/java/com/socicalc/web/IndexController.java
@@ -23,7 +23,7 @@ public class IndexController {
 
     @GetMapping("/")
     public String index(Model model) {
-        model.addAttribute("posts", postsService.findAllDesc());
+        //model.addAttribute("posts", postsService.findAllDesc());
         return "index";
     }
 
